37c4ff4c7ebdd2e24331fdd0ae6b139688830176
Fabion Stephens <fabions@octobits.io>
2018-04-03 13:30:22 -0500

    Started working on skeleton for sign in/up components and get rid of unwanted files

---
+++ src/components/sign-in-up/sign_in.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SignInUpLayout from '../../layout/sign_in_up';
+
+class SignIn extends Component{
+  render() {
+    return (
+      <SignInUpLayout>
+        sign in
+      </SignInUpLayout>
+    )
+  }
+}
+
+export default SignIn;
