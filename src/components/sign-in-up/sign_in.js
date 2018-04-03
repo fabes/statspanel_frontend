e97fcaaa1edd4a1245118b0c4c1f6998727f5031
Fabion Stephens <fabions@octobits.io>
2018-04-03 17:36:36 -0500

    Add react-bootstrap, material-ui packages and start styling sign in component

--- src/components/sign-in-up/sign_in.js
+++ src/components/sign-in-up/sign_in.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import SignInUpLayout from '../../layout/sign_in_up';
 
-class SignIn extends Component{
+class SignIn extends Component {
   render() {
+    const customInputStyles = {
+      forText: {
+        color: '#6b8ab9',
+        fontWeight: 'normal'
+      }
+    }
     return (
       <SignInUpLayout>
-        sign in
+        <Row>
+          <Col xs={12} sm={3} md={4} className="v-spacer"></Col>
+          <Col xs={12} sm={6} md={4} className="v-spacer">
+            <div className="login-body">
+              <Row>
+                <Col xs={12}>
+                  <TextField
+                    name="email"
+                    hintText="Your Email"
+                    hintStyle={customInputStyles.forText}
+                    inputStyle={customInputStyles.forText}
+                    fullWidth={true}
+                  />
+                </Col>
+                <Col xs={12}>
+                  <TextField
+                    type="password"  
+                    name="password"
+                    hintText="Your Password"
+                    hintStyle={customInputStyles.forText}
+                    fullWidth={true}
+                  />
+                </Col>
+                <Col xs={12}>
+                  <RaisedButton
+                    className="v-spacer"  
+                    fullWidth={true}
+                    label="Sign In"
+                    primary={true}
+                  />
+                </Col>
+                <Col xs={12}>
+                  Forgot Your Password?
+                </Col>
+              </Row>
+            </div>
+            <div className="login-footer"></div>
+          </Col>
+          <Col xs={12} sm={3} md={4} className="v-spacer"></Col>
+        </Row>
       </SignInUpLayout>
     )
   }
--- src/layout/sign_in_up.js
+++ src/layout/sign_in_up.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import '../assets/styles/global.css';
 
 class SignInUpLayout extends Component{
   render() {
     return (
-      <div>
-        {this.props.children}
-      </div>
+      <MuiThemeProvider>
+        <div className="container">
+          {this.props.children}
+        </div>  
+      </MuiThemeProvider>
     )
   }
 }
