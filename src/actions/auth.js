c50265ff21fd7fe19dc78af525a61ff6cd517fab
Fabion Stephens <fabions@octobits.io>
2018-04-09 17:54:19 -0500

    Working on user sign_in action to test Rails API backend

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,6 @@
 import { AUTH_CONSTANTS } from '../constants/auth';
+import { app_config } from '../config';
+import history from '../utils/history';
 
 const perform_sign_in = (user_input) => ({
   type: AUTH_CONSTANTS.user_sign_in,
@@ -6,5 +8,27 @@ const perform_sign_in = (user_input) => ({
 })
 
 export const sign_in = (user_input) => {
-  // use fetch
+  return (dispatch, getState) => {
+    let post_data = {
+      method: 'POST',
+      body: JSON.stringify(user_input),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }
+
+    fetch(app_config.base_api_url + 'users/sign_in', post_data)
+      .then(response => response.json())
+      .then(data => {
+        if (data !== '') {
+          localStorage.setItem('auth_token', data.token);
+          dispatch(perform_sign_in(data));
+          history.push('/dashboard');
+        }
+      })
+      .catch(e => {
+        console.log('error =', e);
+      })
+  }
 }
