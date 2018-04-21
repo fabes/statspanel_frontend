d7ef533a05b4ff44c349b3e1acf7adf63b4e05f6
Fabion Stephens <fabions@octobits.io>
2018-04-21 14:54:25 -0500

    redirect user from sign in/up if valid_token

--- src/layout/sign_in_up.js
+++ src/layout/sign_in_up.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import '../assets/styles/global.css';
+import { has_valid_token } from '../actions/auth';
+import history from '../utils/history';
 
 class SignInUpLayout extends Component {
+  componentWillMount() {
+    this.props.actions.has_valid_token();
+
+    if (this.props.valid_user_token === true) {
+      history.push('/dashboard');
+    }
+  }  
   render() {
     return (
       <div className="container">
@@ -11,4 +22,17 @@ class SignInUpLayout extends Component {
   }
 }
 
-export default SignInUpLayout;
+function mapStateToProps(state) {
+  return {
+		valid_user_token: state.users.valid_user_token,
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      has_valid_token,
+    }, dispatch)
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(SignInUpLayout);
