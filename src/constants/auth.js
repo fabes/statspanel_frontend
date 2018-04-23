4b43ddfc1c717898ab49d736fd30eee8b306626d
Fabion Stephens <fabions@octobits.io>
2018-04-23 13:47:45 -0500

    Implement logout functionality to update redux states

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -2,4 +2,6 @@ export const AUTH_CONSTANTS = {
   user_sign_in: 'USER_SIGN_IN',
   user_sign_up: 'USER_SIGN_UP',
   check_valid_token: 'CHECK_VALID_TOKEN',
+  set_invalid_token: 'SET_INVALID_TOKEN',
+  reset_current_user: 'RESET_CURRENT_USER',
 }
